635afa573ab43898c2c337d60d5a26f202c6cdf0
ift_praveen <ift_praveen@mcinc.jp>
2020-05-04 19:28:57 +0900

    created dev environment

--- app/src/main/java/jp/mcinc/imesh/type/ipphone/activity/VoiceActivity.java
+++ app/src/main/java/jp/mcinc/imesh/type/ipphone/activity/VoiceActivity.java
@@ -108,7 +108,8 @@ public class VoiceActivity extends AppCompatActivity {
      * If your token server is written in PHP, TWILIO_ACCESS_TOKEN_SERVER_URL needs .php extension at the end.
      *
      * For example : https://myurl.io/accessToken.php
-     * praveen deshmane
+     * praveen deshmane 123
+     *
      */
     private static final String TWILIO_REGISTRATION_URL = "https://toolbox-gnat-5377.twil.io/register-binding";
     private static final String TWILIO_ACCESS_TOKEN_SERVER_URL = "TWILIO_ACCESS_TOKEN_SERVER_URL";
